14853887c0abe6e94c1dc6b84ea4b9e065b48041
stephen <stephen.r.rayner@gmail.com>
2022-09-22 18:59:55 +0100

    init

---
+++ serverless.ts
@@ -0,0 +1,31 @@
+import type { AWS } from '@serverless/typescript';
+
+import { hello } from './src/functions';
+
+const serverlessConfiguration: AWS = {
+  service: 'serverless-workspaces-pnp',
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true,
+      packager: "yarn",
+    }
+  },
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    lambdaHashingVersion: '20201221',
+  },
+  functions: { hello }
+}
+
+module.exports = serverlessConfiguration;
